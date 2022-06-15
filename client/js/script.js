88ad416505df93cbf5f4a26b59f6996389dde3c8
emanuel <emanuel1999emy@gmail.com>
2022-06-15 17:10:24 +0200

    commit posts

--- client/js/script.js
+++ client/js/script.js
@@ -40,25 +40,23 @@ fetch('http://localhost:3000/users')
         usersDiv.innerHTML = `${ele.name}${'<br>'}${ele.email}${'<br>'}${ele.phone}${'<br>'}${ele.website}${'<br>'}${ele.address.street}${'<br>'}${ele.address.city}${'<br>'}`
         section.appendChild(usersDiv);
 
-        /* -------------- Posts div ---------------- */
+        /* -------------- Buttons div ---------------- */
 
-        fetch('http://localhost:3000/users/'+(++index)+'/posts')
-        .then(res => res.json())    // json parse per trasformare i dati dal json
-        .then(function(post) {
-            let postsDiv = document.createElement('div');
-            postsDiv.className = 'postsDiv';
-            post.forEach((ele1)=>{
-                let titolo = document.createElement('h2')
-                titolo.innerText = ele1.title
-                postsDiv.appendChild(titolo)
-                let testo_post = document.createElement('p')
-                testo_post.innerText = ele1.body
-                postsDiv.appendChild(testo_post)
-            })
-            section.appendChild(postsDiv);
+        let btnDiv = document.createElement('div');
+        btnDiv.className = 'btnDiv';
 
-            cardCont.appendChild(postsDiv);
-        })     // data contiene i dati fetchati dal json
+        let btnAlbum = document.createElement('button');
+        btnAlbum.className = 'btnAlbum btn btn-dark';
+        btnAlbum.innerText = 'album'
+        btnDiv.appendChild(btnAlbum);
+
+        let btnToDo = document.createElement('button');
+        btnToDo.className = 'btnToDo btn btn-dark';
+        btnToDo.innerText = 'todolist';
+        btnDiv.appendChild(btnToDo);
+
+
+
 
         /* -------------- Image div ---------------- */
 
@@ -71,43 +69,62 @@ fetch('http://localhost:3000/users')
         /* imgDiv.innerHTML = '<img src=" '+ ele.profileURL +' ">'; */ // ID DA SOSTITUIRE! creo tag img nell'html e compilo con id json dell'img
         section.appendChild(imgDiv);
         
-        /* -------------- Buttons div ---------------- */
 
-        let btnDiv = document.createElement('div');
-        btnDiv.className = 'btnDiv';
-
-        let btnComments = document.createElement('button');
-        btnComments.className = 'btnComments btn btn-dark';
-        btnComments.innerText = 'post'
-        btnDiv.appendChild(btnComments);
-
-        let btnAlbum = document.createElement('button');
-        btnAlbum.className = 'btnAlbum btn btn-dark';
-        btnAlbum.innerText = 'album'
-        btnDiv.appendChild(btnAlbum);
-
-        let btnToDo = document.createElement('button');
-        btnToDo.className = 'btnToDo btn btn-dark';
-        btnToDo.innerText = 'todolist';
-        btnDiv.appendChild(btnToDo);
 
         /* ----- Comments/Album/ToDoList Display ----- */
 
         let displayDiv = document.createElement('div');
         displayDiv.className = 'displayDiv';
 
-        let displayComments = document.createElement('div');
-        displayComments.className = 'displayComments';
-        displayDiv.appendChild(displayComments);
+        /* -------------- Posts div ---------------- */
 
-        let displayAlbum = document.createElement('div');
-        displayAlbum.className = 'displayAlbum';
-        displayDiv.appendChild(displayAlbum);
+        let btnPosts = document.createElement('div');
+        btnPosts.className = 'd-inline'
+        btnPosts.innerHTML = '<button class="btn btn-dark" type="button">posts</button>' 
+        btnPosts.addEventListener('click',()=>{
+            displayDiv.innerHTML = ''
+            let num_post = index+1
+            if (document.getElementsByClassName('displayDiv')[index].classList.contains('show')){
+                document.getElementsByClassName('displayDiv')[index].classList.add('no-show')
+                document.getElementsByClassName('displayDiv')[index].classList.remove('show')
+            }else{
+                document.getElementsByClassName('displayDiv')[index].classList.add('show') 
+                document.getElementsByClassName('displayDiv')[index].classList.remove('no-show')
+            }
+            fetch('http://localhost:3000/users/'+num_post+'/posts')
+            .then(res => res.json())    // json parse per trasformare i dati dal json
+            .then(function(post) {
+            let postsDiv = document.createElement('div');
+            postsDiv.className = 'postsDiv';
+            post.forEach((ele1 , ele1_index)=>{
+                let titolo = document.createElement('h2')
+                titolo.innerText = ele1.title
+                postsDiv.appendChild(titolo)
+                let testo_post = document.createElement('p')
+                testo_post.innerText = ele1.body
+                postsDiv.appendChild(testo_post)
+                let div_commenti = document.createElement('div')
+                fetch('http://localhost:3000/posts/'+(ele1_index+1)+'/comments')
+                .then(res => res.json())    // json parse per trasformare i dati dal json
+                .then(function(commenti) {
+                    commenti.forEach((commento)=>{
+                        console.log(div_commenti)
+                        let titolo_commenti = document.createElement('h3')
+                        titolo_commenti.innerText = commento.email
+                        div_commenti.appendChild(titolo)
+                        let testo_commenti = document.createElement('p')
+                        testo_commenti.innerText = commento.body
+                        div_commenti.appendChild(testo_post)
+                    })
 
-        let displayToDo = document.createElement('div');
-        displayToDo.className = 'displayToDo';
-        displayDiv.appendChild(displayToDo);
-
+                })
+                displayDiv.appendChild(div_commenti)
+            })
+            section.appendChild(postsDiv);
+            displayDiv.appendChild(postsDiv);
+            })     // data contiene i dati fetchati dal json
+        })
+        btnDiv.appendChild(btnPosts);
         /* -------------- Arrows Div ----------------- */
 
         let arrowsDiv = document.createElement('div');
